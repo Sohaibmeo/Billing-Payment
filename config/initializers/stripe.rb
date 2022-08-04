6189b24b7133a57f0a6f36be9c6c6d50835b61e3
Sohaib Mao <sohaib.mmeo@devsinc.com>
2022-08-04 15:57:11 +0500

    Bug Fixes 2

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -5,9 +5,5 @@ Rails.configuration.stripe = {
   publishable_key: Rails.application.credentials.STRIPE_PUBLISHABLE_KEY,
   secret_key: Rails.application.credentials.STRIPE_SECRET_KEY
 }
-# Rails.application.configure do
-#   config.stripe.secret_key = Rails.application.credentials.STRIPE_SECRET_KEY
-#   config.stripe.publishable_key = Rails.application.credentials.STRIPE_PUBLISHABLE_KEY
-# end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
--- db/migrate/20220804093215_add_reference_to_feature_use.rb
+++
@@ -1,5 +0,0 @@
-class AddReferenceToFeatureUse < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :feature_uses
-  end
-end
