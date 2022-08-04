e7e05254b72b94231ee4e0b24943fe954842b324
Sohaib Mao <sohaib.mmeo@devsinc.com>
2022-08-04 22:32:34 +0500

    herokufix#2

---
+++ db/migrate/20220804173001_add_featureuse_table.rb
@@ -0,0 +1,9 @@
+class AddFeatureuseTable < ActiveRecord::Migration[7.0]
+    def change
+      create_table :feature_uses do |t|
+        t.integer :total_units
+        t.references :usage, :feature, null: false, foreign_key: true
+        t.timestamps
+      end
+    end
+end
