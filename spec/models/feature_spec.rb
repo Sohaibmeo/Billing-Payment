10d64e1c4f9e12eae4d917f4a468e68510d15893
Sohaib Mao <sohaib.mmeo@devsinc.com>
2022-08-23 01:30:43 +0500

    test cases: model

--- spec/models/feature_spec.rb
+++ spec/models/feature_spec.rb
@@ -1,31 +1,31 @@
-# # frozen_string_literal: true
+# frozen_string_literal: true
 
-# require 'rails_helper'
-# require 'simplecov'
-# SimpleCov.start
+require 'rails_helper'
+require 'simplecov'
+SimpleCov.start
 
-# RSpec.describe Feature, type: :model do
-#   let(:feature) { described_class.new }
+RSpec.describe Feature, type: :model do
+  let(:feature) { described_class.new }
 
-#   it { expect(feature).to validate_numericality_of(:max_unit_limit).is_greater_than(0) }
+  it { expect(feature).to validate_numericality_of(:max_unit_limit).is_greater_than(0) }
 
-#   it { expect(feature).to validate_numericality_of(:unit_price).is_greater_than(0) }
+  it { expect(feature).to validate_numericality_of(:unit_price).is_greater_than(0) }
 
-#   it { expect(feature).to validate_uniqueness_of :name }
+  it { expect(feature).to validate_uniqueness_of :name }
 
-#   it { expect(feature).to validate_uniqueness_of :code }
+  it { expect(feature).to validate_uniqueness_of :code }
 
-#   it { expect(feature).to validate_presence_of :name }
+  it { expect(feature).to validate_presence_of :name }
 
-#   it { expect(feature).to validate_presence_of :code }
+  it { expect(feature).to validate_presence_of :code }
 
-#   it { expect(feature).to validate_length_of(:code).is_at_most(6) }
+  it { expect(feature).to validate_length_of(:code).is_at_most(6) }
 
-#   it { expect(feature).to have_many(:plans) }
+  it { expect(feature).to have_many(:plans) }
 
-#   it { expect(feature).to have_many(:feature_uses) }
+  it { expect(feature).to have_many(:feature_uses) }
 
-#   it { expect(feature).to have_many(:usages) }
+  it { expect(feature).to have_many(:usages) }
 
-#   it { expect(feature).to have_many(:items) }
-# end
+  it { expect(feature).to have_many(:items) }
+end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,25 +1,25 @@
-# # frozen_string_literal: true
+# frozen_string_literal: true
 
-# require 'rails_helper'
-# require 'simplecov'
-# SimpleCov.start
+require 'rails_helper'
+require 'simplecov'
+SimpleCov.start
 
-# RSpec.describe Item, type: :model do
-#   let(:plan) { Plan.new(name: 'TestPlan', price: 20) }
+RSpec.describe Item, type: :model do
+  let(:plan) { Plan.new(name: 'TestPlan', price: 20) }
 
-#   let(:feature) { Feature.new(name: 'TestFeature2', code: 1234, max_unit_limit: 1, unit_price: 1) }
+  let(:feature) { Feature.new(name: 'TestFeature2', code: 1234, max_unit_limit: 1, unit_price: 1) }
 
-#   let(:item) { described_class.new(feature_id: feature.id, plan_id: plan.id) }
+  let(:item) { described_class.new(feature_id: feature.id, plan_id: plan.id) }
 
-#   it { expect(item).to belong_to(:feature) }
+  it { expect(item).to belong_to(:feature) }
 
-#   it { expect(item).to belong_to(:plan) }
+  it { expect(item).to belong_to(:plan) }
 
-#   it 'has a unique feature with scoped plan_id' do
-#     plan.save
-#     feature.save
-#     item.save
-#     item2 = described_class.new(feature_id: feature.id, plan_id: plan.id)
-#     expect(item2).to be_invalid
-#   end
-# end
+  it 'has a unique feature with scoped plan_id' do
+    plan.save
+    feature.save
+    item.save
+    item2 = described_class.new(feature_id: feature.id, plan_id: plan.id)
+    expect(item2).to be_invalid
+  end
+end
